eb91ebb2eb0997763dc25acc8459ca661a780fca
ola <oola.olawoyin@gmail.com>
2026-06-20 23:58:07 +0100

    add procfile

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vu6k4lioni%y4r*_a++y#(#^9+xrz@&p++5wyjtcz5a3u53sbd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
